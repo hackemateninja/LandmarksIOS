1e35d4fc23758afc269376637932de1959921e9f
Herman <nitrowitty@gmail.com>
2023-09-18 14:53:58 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Herman Orlando Morales Lòpez on 18/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
